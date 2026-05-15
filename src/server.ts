60387d134b4192882b032c9aaf3e399a33016351
igorDCat <powerwolf891028@gmail.com>
2026-05-15 13:59:22 +0200

    added cors

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,18 @@
 import 'dotenv/config';
 
 import express from 'express';
+import cors from 'cors';
 import checkerRoutes from './routes/checkerRoutes';
 
 const app = express();
 
-app.use(express.json());
+app.use(cors({
+    origin: 'http://localhost:5173',
+}));
 
 app.use('/checker', checkerRoutes);
+
+app.use(express.json());
 
 app.get('/', (_, res) => {
     res.json({
